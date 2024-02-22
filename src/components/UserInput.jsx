71c72919dfb289a97eb02fd4567ec19c9395a0e9
serotonine <ks.serotonine@gmail.com>
2024-02-22 11:40:59 +0100

    Remove unusued code.

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,25 +1,4 @@
 import Input from "./Input";
-import { Fragment } from "react";
-/* function getInputs(datas, onChangeValue) {
-  const _datas = Object.entries(datas);
-  const markup = _datas.map((data, index) => {
-    const isWrap = index % 2 === 0;
-   
-    
-    return 
-    ({isWrap && }
-      <Input
-        index={data[0]}
-        label={data[0]}
-        value={data[1]}
-        onChangeValue={onChangeValue}
-      />
-      {isWrap && </div>})
-     
-   
-  });
-  return markup;
-} */
 
 export default function UserInput({ datas, onChangeValue }) {
   const _datas = Object.entries(datas);
